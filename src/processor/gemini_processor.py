c7d819563180bc267277644c29e52d09a6dab31c
Jean <jtrujillo@galgo.com>
2026-01-02 09:29:05 -0500

    feat(): Enviar prompt a Gemini y obtener su respuesta

---
+++ src/processor/gemini_processor.py
@@ -0,0 +1,21 @@
+from google import genai
+
+class GeminiProcessor:
+    def __init__(self):
+        self.client = genai.Client()
+        self.model = 'gemini-3-pro-preview'
+
+    def send_prompt(self, prompt:str) -> str:
+        interaction = self.client.interactions.create(
+            model=self.model,
+            input=prompt,
+            tools=[{'type': 'google_search'}]
+        )
+        return self.get_response(interaction)
+
+    def get_response(self, interaction) -> str:
+        final_output = ""
+        for output in interaction.outputs:
+            if hasattr(output, "text"):
+                final_output += output.text
+
