4bf0802625fc51736c8ce99127dd278b3dc2caed
Jean <jtrujillo@galgo.com>
2026-02-20 15:48:16 -0500

    update

--- src/utils/datetime_utils.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-def get_current_date_format_yyyy_mm_dd() -> str:
-    """
-    Obtiene la fecha actual en formato YYYY-MM-DD
-    """
-    return datetime.now().strftime("%Y-%m-%d")
